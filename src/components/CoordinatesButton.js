dbcf5ed87180e14a9489825482cff69e1fb2236d
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-08-11 15:45:08 +1000

    Completed Assignment

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,14 @@
-// Code CoordinatesButton Component Here
+import React, {Component} from 'react'
+
+export default class CoordinatesButton extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        //console.log(this.props)
+        return(
+            <button onClick = {this.props.onReceiveCoordinates}></button>
+        )
+    }
+}
